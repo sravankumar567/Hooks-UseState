9a32a9ebd90420018dd0e94d926c36728370cfe1
sravankumar567 <119730950+sravankumar567@users.noreply.github.com>
2023-01-28 16:58:01 +0530

    Add files via upload
    Creating a counter using the react hook useState('')
    const [count,setCount]=useState('')
    useState() -> the useState hook is a special function that takes the initial state as a an argument and returns an array of two entities

---
+++ HooksState.js
@@ -0,0 +1,16 @@
+import React ,{useState} from 'react'
+
+function HooksState() {
+    const[count,setcount]=useState(0);
+    
+    
+    
+  return (
+    <div>
+        <button onClick={()=>setcount(count+1)}>count  {count}</button>
+    </div>
+  )
+}
+
+
+export default HooksState
